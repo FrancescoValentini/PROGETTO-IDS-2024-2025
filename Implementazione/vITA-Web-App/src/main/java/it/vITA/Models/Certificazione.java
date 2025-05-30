81a1c2f1768d057aa2e7f442ff484764aad5f058
Giulia Balestra <balestrag9@gmail.com>
2025-05-30 16:51:08 +0200

    Creato modello Certificazione

---
+++ Implementazione/vITA-Web-App/src/main/java/it/vITA/Models/Certificazione.java
@@ -0,0 +1,78 @@
+package it.vITA.Models;
+
+
+import java.time.LocalDateTime;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="CERTIFICAZIONI")
+public class Certificazione {
+	@Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", columnDefinition = "VARCHAR(64)")
+	private String id;
+	private String denominazione;
+	private String descrizione;
+	private String denominazioneEnteCertificatore;
+	private LocalDateTime dataConseguimento;
+	private LocalDateTime dataScadenza;
+	// private ArrayList<File> allegati;
+	
+	public Certificazione() {}
+	public Certificazione(String denominazione, String descrizione, String denominazioneEnteCertificatore,
+			LocalDateTime dataConseguimento, LocalDateTime dataScadenza) {
+		this.denominazione = denominazione;
+		this.descrizione = descrizione;
+		this.denominazioneEnteCertificatore = denominazioneEnteCertificatore;
+		this.dataConseguimento = dataConseguimento;
+		this.dataScadenza = dataScadenza;
+	}
+	public String getDenominazione() {
+		return denominazione;
+	}
+	public void setDenominazione(String denominazione) {
+		this.denominazione = denominazione;
+	}
+	public String getDescrizione() {
+		return descrizione;
+	}
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+	public String getDenominazioneEnteCertificatore() {
+		return denominazioneEnteCertificatore;
+	}
+	public void setDenominazioneEnteCertificatore(String denominazioneEnteCertificatore) {
+		this.denominazioneEnteCertificatore = denominazioneEnteCertificatore;
+	}
+	public LocalDateTime getDataConseguimento() {
+		return dataConseguimento;
+	}
+	public void setDataConseguimento(LocalDateTime dataConseguimento) {
+		this.dataConseguimento = dataConseguimento;
+	}
+	public LocalDateTime getDataScadenza() {
+		return dataScadenza;
+	}
+	public void setDataScadenza(LocalDateTime dataScadenza) {
+		this.dataScadenza = dataScadenza;
+	}
+	public String getId() {
+		return id;
+	}
+	@Override
+	public String toString() {
+		return "Certificazione [id=" + id + ", denominazione=" + denominazione + ", descrizione=" + descrizione
+				+ ", denominazioneEnteCertificatore=" + denominazioneEnteCertificatore + ", dataConseguimento="
+				+ dataConseguimento + ", dataScadenza=" + dataScadenza + "]";
+	}
+	
+	
+	
+	
+}
