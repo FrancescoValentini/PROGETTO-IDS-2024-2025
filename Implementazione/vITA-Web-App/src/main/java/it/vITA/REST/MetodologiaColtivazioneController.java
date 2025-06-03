30b4ba5080cbbad17372bc86bdaf5ff1b29c88ec
Giulia Balestra <balestrag9@gmail.com>
2025-06-03 20:54:04 +0200

    Controller MetodologiaColtivazione
    get metodologia by id
    Create metodologia

--- Implementazione/vITA-Web-App/src/main/java/it/vITA/REST/MetodologiaColtivazioneController.java
+++ Implementazione/vITA-Web-App/src/main/java/it/vITA/REST/MetodologiaColtivazioneController.java
@@ -17,6 +17,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import it.vITA.DTO.MetodologiaColtivazioneDTO;
+import it.vITA.DTO.PosizioneDTO;
 import it.vITA.Models.MetodologiaColtivazione;
 import it.vITA.Models.Posizione;
 import it.vITA.Repositories.MetodologieColtivazioneRepository;
@@ -24,7 +26,7 @@ import it.vITA.Repositories.MetodologieColtivazioneRepository;
  * Rest controller che gestisce le metodologie di coltivazione
  */
 @RestController
-@RequestMapping("/api/metodologie")
+@RequestMapping("/api/metodologia")
 public class MetodologiaColtivazioneController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(MetodologiaColtivazioneController.class);
@@ -43,6 +45,33 @@ public class MetodologiaColtivazioneController {
 		List<MetodologiaColtivazione> met = new ArrayList<>();
 		metodologie.forEach(x -> met.add(x));
 		return new ResponseEntity<>(metodologie,HttpStatus.OK);
+	}
+	
+	/**
+	 * Restituisce una singola metodologia dato il suo id
+	 * 
+	 * @param id della metodologia
+	 * @author Giulia Balestra
+	 */
+	@GetMapping("/{id}")
+	public ResponseEntity<Object> getMetodologia(@PathVariable("id") String id) {
+		if(repoMetodologie.existsById(id)) {
+			return new ResponseEntity<>(repoMetodologie.findById(id).get(),HttpStatus.OK);
+		}
+		return new ResponseEntity<>("Metodologia non trovata",HttpStatus.NOT_FOUND);
+	}
+	
+	/**
+	 * Aggiunge una nuova metodologia
+	 * @param DTOMetodologiaColtivazioneDTO
+	 * @author Giulia Balestra
+	 */
+	@PostMapping
+	public ResponseEntity<Object> createMetodologia(@RequestBody MetodologiaColtivazioneDTO metodologia){
+		MetodologiaColtivazione met = new MetodologiaColtivazione(metodologia.getDenominazione(), 
+				metodologia.getDescrizione());
+		repoMetodologie.save(met);
+		return new ResponseEntity<>(met,HttpStatus.CREATED);
 	}
 	
 }
