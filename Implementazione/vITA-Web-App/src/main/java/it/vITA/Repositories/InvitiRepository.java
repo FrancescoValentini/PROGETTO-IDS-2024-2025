c12a9e3b0934e9846d9731ce7d5b46128bd9962a
Francesco Valentini <Francesco.valentini930@gmail.com>
2025-06-11 17:26:57 +0200

    Bugfix Controller Invito

--- Implementazione/vITA-Web-App/src/main/java/it/vITA/Repositories/InvitiRepository.java
+++ Implementazione/vITA-Web-App/src/main/java/it/vITA/Repositories/InvitiRepository.java
@@ -1,9 +1,14 @@
 package it.vITA.Repositories;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import it.vITA.Models.Invito;
 
 public interface InvitiRepository extends CrudRepository<Invito, String> {
-
+	@Modifying
+	@Query("DELETE FROM Invito i WHERE i.id = :id")
+	void deleteInvitoById(@Param("id") String id);
 }
