40736908878d04c75176e4c697957f939347c17f
Francesco Valentini <Francesco.valentini930@gmail.com>
2025-06-15 14:50:24 +0200

    ProdottoDTO

---
+++ Implementazione/vITA-Web-App/src/main/java/it/vITA/DTO/ProdottoDTO.java
@@ -0,0 +1,60 @@
+package it.vITA.DTO;
+
+import java.time.LocalDateTime;
+
+import it.vITA.Models.Produttore;
+
+public class ProdottoDTO {
+	  private String denominazione;
+	  private String descrizione;
+	  private LocalDateTime dataEoraProduzione;
+	  private LocalDateTime dataEoraScadenza;
+	  private Produttore produttore;
+	/**
+	 * @param denominazione
+	 * @param descrizione
+	 * @param dataEoraProduzione
+	 * @param dataEoraScadenza
+	 * @param produttore
+	 */
+	public ProdottoDTO(String denominazione, String descrizione, LocalDateTime dataEoraProduzione,
+			LocalDateTime dataEoraScadenza, Produttore produttore) {
+		this.denominazione = denominazione;
+		this.descrizione = descrizione;
+		this.dataEoraProduzione = dataEoraProduzione;
+		this.dataEoraScadenza = dataEoraScadenza;
+		this.produttore = produttore;
+	}
+	public String getDenominazione() {
+		return denominazione;
+	}
+	public void setDenominazione(String denominazione) {
+		this.denominazione = denominazione;
+	}
+	public String getDescrizione() {
+		return descrizione;
+	}
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+	public LocalDateTime getDataEoraProduzione() {
+		return dataEoraProduzione;
+	}
+	public void setDataEoraProduzione(LocalDateTime dataEoraProduzione) {
+		this.dataEoraProduzione = dataEoraProduzione;
+	}
+	public LocalDateTime getDataEoraScadenza() {
+		return dataEoraScadenza;
+	}
+	public void setDataEoraScadenza(LocalDateTime dataEoraScadenza) {
+		this.dataEoraScadenza = dataEoraScadenza;
+	}
+	public Produttore getProduttore() {
+		return produttore;
+	}
+	public void setProduttore(Produttore produttore) {
+		this.produttore = produttore;
+	}
+	  
+	  
+}
