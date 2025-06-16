1444619e5cc51e6adf5ab01ea35dc69388b41865
Giorgio Pranzetti <giorgio.prn07@gmail.com>
2025-06-16 13:49:26 +0200

    Curatore

---
+++ Implementazione/vITA-Web-App/src/main/java/it/vITA/Models/Curatore.java
@@ -0,0 +1,11 @@
+package it.vITA.Models;
+
+public class Curatore extends UtenteRegistrato {
+	/*
+	 * Classe vuota per eseguire implementazione
+	 * di CuratoreDTO e
+	 * CuratoreController
+	 * 
+	 */
+
+}
