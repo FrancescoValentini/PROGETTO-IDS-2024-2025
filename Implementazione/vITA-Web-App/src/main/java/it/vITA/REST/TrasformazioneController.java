162166c7e67fa08949085ba401267a37924c26b2
Giulia Balestra <balestrag9@gmail.com>
2025-06-16 08:53:21 +0200

    TrasformazioneController CreateTrasformazione

--- Implementazione/vITA-Web-App/src/main/java/it/vITA/REST/TrasformazioneController.java
+++ Implementazione/vITA-Web-App/src/main/java/it/vITA/REST/TrasformazioneController.java
@@ -1,5 +1,6 @@
 package it.vITA.REST;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,10 +12,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import it.vITA.DTO.TrasformazioneDTO;
 import it.vITA.Models.Trasformazione;
+import it.vITA.Repositories.TrasformatoreRepository;
 import it.vITA.Repositories.TrasformazioniRepository;
 
 
@@ -28,10 +32,14 @@ import it.vITA.Repositories.TrasformazioniRepository;
 @RestController
 @RequestMapping("/api/trasformazione")
 public class TrasformazioneController {
-private static final Logger logger = LoggerFactory.getLogger(TrasformazioneController.class);
 	
 	@Autowired
 	TrasformazioniRepository repoTrasformazioni;
+	@Autowired
+	TrasformatoreRepository repoTrasformatori;
+	
+private static final Logger logger = LoggerFactory.getLogger(TrasformazioneController.class);
+	
 	
 	
 	/**
@@ -47,6 +55,7 @@ private static final Logger logger = LoggerFactory.getLogger(TrasformazioneContr
 	/**
 	 * Restituisce una singola trasformazione dato il suo id
 	 * @param id allergene
+	 * @param id trasformazione
 	 */
 	@GetMapping("/{id}")
 	public ResponseEntity<Object> getTrasformazione(@PathVariable("id") String id) {
@@ -54,6 +63,35 @@ private static final Logger logger = LoggerFactory.getLogger(TrasformazioneContr
 			return new ResponseEntity<>(repoTrasformazioni.findById(id).get(),HttpStatus.OK);
 		}
 		return new ResponseEntity<>("Trasformazione non trovata",HttpStatus.NOT_FOUND);
+	}
+	
+	/**
+	 * Crea una nuova trasformazione
+	 * @param TrasformazioneDTO
+	 * @return trasformazione aggiunta
+	 */
+	
+	@PostMapping
+	public ResponseEntity<Object> createTrasformazione(@RequestBody TrasformazioneDTO dto) {
+
+	    Trasformazione t = null;
+
+	    boolean trasformatoreExist = repoTrasformatori.existsById(dto.getIdTrasformatore());
+
+	    if (trasformatoreExist) {
+	        t = new Trasformazione();
+	        t.setDenominazione(dto.getDenominazione());
+	        t.setDescrizione(dto.getDescrizione());
+	        t.setDataInizioFase(LocalDateTime.now());
+	        t.setDataFineFase(dto.getDataFineFase());
+	        t.setTrasformatore(repoTrasformatori.findById(dto.getIdTrasformatore()).get());
+	        t.setCertificazioni(dto.getCertificazioni());
+
+	        repoTrasformazioni.save(t);
+	        return new ResponseEntity<>(t, HttpStatus.CREATED);
+	    }
+
+	    return new ResponseEntity<>("Trasformatore non trovato", HttpStatus.NOT_FOUND);
 	}
 	
 	
