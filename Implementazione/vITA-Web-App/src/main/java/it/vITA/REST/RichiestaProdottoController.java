23539a38570a9fdb4b7f461757ea8ffb95676390
Giulia Balestra <balestrag9@gmail.com>
2025-06-17 22:23:22 +0200

    Update - Delete Richiesta Prodotto Controller

--- Implementazione/vITA-Web-App/src/main/java/it/vITA/REST/RichiestaProdottoController.java
+++ Implementazione/vITA-Web-App/src/main/java/it/vITA/REST/RichiestaProdottoController.java
@@ -6,9 +6,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -87,5 +89,61 @@ public class RichiestaProdottoController {
 	        repoRichiestaP.save(richiesta);
 	        return new ResponseEntity<>(richiesta, HttpStatus.CREATED);
 	    }
+	    
+	    /**
+	     * Aggiorna una richiesta prodotto esistente
+	     * @param id
+	     * @param dti
+	     * @return richiesta prodotto aggiornata
+	     */
+	    @PutMapping("/{id}")
+	    public ResponseEntity<Object> updateRichiestaProdotto(@PathVariable String id, @RequestBody RichiestaProdottoDTO dto) {
+	        if (!repoRichiestaP.existsById(id)) {
+	            return new ResponseEntity<>("Richiesta prodotto non trovata", HttpStatus.NOT_FOUND);
+	        }
+
+	        RichiestaProdotto richiesta = repoRichiestaP.findById(id).get();
+
+	        if (dto.getIdCreatore() != null) {
+	            if (!repoUtente.existsById(dto.getIdCreatore())) {
+	                return new ResponseEntity<>("Creatore non trovato", HttpStatus.NOT_FOUND);
+	            }
+	            UtenteRegistrato creatore = repoUtente.findById(dto.getIdCreatore()).get();
+	            richiesta.setCreatore(creatore);
+	        }
+
+	        if (dto.getIdProdotto() != null) {
+	            if (!repoProdotto.existsById(dto.getIdProdotto())) {
+	                return new ResponseEntity<>("Prodotto non trovato", HttpStatus.NOT_FOUND);
+	            }
+	            Prodotto prodotto = repoProdotto.findById(dto.getIdProdotto()).get();
+	            richiesta.setProdotto(prodotto);
+	        }
+
+	        if (dto.getCommentoCuratore() != null) {
+	            richiesta.setCommentoCuratore(dto.getCommentoCuratore());
+	        }
+
+	        richiesta.setApprovato(dto.isApprovato());
+
+	        if (dto.getTipoRichiesta() != null) {
+	            richiesta.setTipoRichiesta(dto.getTipoRichiesta());
+	        }
+
+	        repoRichiestaP.save(richiesta);
+
+	        return new ResponseEntity<>(richiesta, HttpStatus.OK);
+	    }
+
+	    @DeleteMapping("/{id}")
+	    public ResponseEntity<Object> deleteRichiestaProdotto(@PathVariable String id) {
+	        if (!repoRichiestaP.existsById(id)) {
+	            return new ResponseEntity<>("Richiesta prodotto non trovata", HttpStatus.NOT_FOUND);
+	        }
+	        repoRichiestaP.deleteById(id);
+	        return new ResponseEntity<>("Richiesta prodotto eliminata", HttpStatus.OK);
+	    }
+
+	    
 	    
 }
