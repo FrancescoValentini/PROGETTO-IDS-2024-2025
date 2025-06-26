de681926b44e1a04f8d33dda5a0c00745ac8a15c
Francesco Valentini <Francesco.valentini930@gmail.com>
2025-06-26 15:18:53 +0200

    Bugfix

--- Implementazione/vITA-Web-App/src/main/java/it/vITA/REST/MarketplaceController.java
+++ Implementazione/vITA-Web-App/src/main/java/it/vITA/REST/MarketplaceController.java
@@ -1,8 +1,10 @@
 package it.vITA.REST;
 
+import it.vITA.DTO.AcquistoDTO;
 import it.vITA.Models.ProdottoInVendita;
 import it.vITA.Repositories.ProdottiInVenditaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,6 +24,38 @@ public class MarketplaceController {
     /**
      * Restituisce tutti i prodotti in vendita il cui prodotto è approvato
      */
+    
+    @PostMapping("/compra/{id}")
+	public ResponseEntity<Object> compraProdotto(@PathVariable String id, @RequestBody AcquistoDTO acquisto) {
+	    int qtaRichiesta = acquisto.getQta();
+	    
+	    // Controllo quantità negativa
+	    if (qtaRichiesta < 1) {
+	        return new ResponseEntity<>("Quantità negativa non consentita", HttpStatus.BAD_REQUEST);
+	    }
+
+	    if (!prodottiInVenditaRepository.existsById(id)) {
+	        return new ResponseEntity<>("Prodotto non trovato", HttpStatus.NOT_FOUND);
+	    }
+
+	    ProdottoInVendita prodotto = prodottiInVenditaRepository.findById(id).get();
+
+	    if (qtaRichiesta <= 0) {
+	        return new ResponseEntity<>("Quantità richiesta non valida", HttpStatus.BAD_REQUEST);
+	    }
+
+	    if (prodotto.getQta() < qtaRichiesta) {
+	        return new ResponseEntity<>("Quantità non disponibile", HttpStatus.BAD_REQUEST);
+	    }
+
+	    prodotto.setQta(prodotto.getQta() - qtaRichiesta);
+	    prodottiInVenditaRepository.save(prodotto);
+
+	    double prezzoTotale = qtaRichiesta * prodotto.getPrezzo();
+
+	    return new ResponseEntity<>("Acquisto completato. Prezzo totale: €" + prezzoTotale, HttpStatus.OK);
+	}
+    
     @GetMapping
     public ResponseEntity<List<ProdottoInVendita>> getProdottiInVenditaApprovati() {
         List<ProdottoInVendita> prodottiApprovati = ((List<ProdottoInVendita>) prodottiInVenditaRepository.findAll())
