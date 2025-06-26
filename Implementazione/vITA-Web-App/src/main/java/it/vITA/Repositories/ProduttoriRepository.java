1bb6342502a32dc9d0a08fc304347043d3590fd0
Giulia Balestra <balestrag9@gmail.com>
2025-06-26 17:55:27 +0200

    RepoProduttori

---
+++ Implementazione/vITA-Web-App/src/main/java/it/vITA/Repositories/ProduttoriRepository.java
@@ -0,0 +1,9 @@
+package it.vITA.Repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import it.vITA.Models.Produttore;
+
+public interface ProduttoriRepository extends CrudRepository<Produttore, String>{
+
+}
